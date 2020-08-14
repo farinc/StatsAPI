903903d5eef7d62e7ce18ade0952832ee1f5ff9e
farinc <farincorperated@gmail.com>
2020-08-14 14:38:24 -0700

    initial commit

---
+++ src/main/java/com/farinc/stats/StatsMain.java
@@ -0,0 +1,10 @@
+package com.farinc.stats;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = StatsMain.MODID)
+public class StatsMain {
+    
+    public static final String MODID = "statsapi";
+
+}
